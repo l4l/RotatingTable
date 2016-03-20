cd4cef93aff0c0ca48f39700561c17f3dd7527b5
Kitsu <mail@kitsu.me>
2016-03-20 19:35:55 +0300

    State creation added

--- src/table/State.cpp
+++ src/table/State.cpp
@@ -1,35 +1,113 @@
 #include "State.h"
 
-State::State(int32_t size) : width(size), height(size) { }
+State::State(int32_t size) : State(size, size) { }
+
+State::State(int32_t w, int32_t h) : width(w), height(h) {
+    field = new int32_t*[width];
+    for (int i = 0; i < width; ++i) {
+        field[i] = new int32_t[height];
+    }
+}
+
+State::~State() {
+    for (int i = 0; i < width; ++i) {
+        delete[] (field[i]);
+    }
+    delete[] field;
+}
 
 // Just copy everything
-State::State(const State& st) : 
-        width(st.width),
-        height(st.height), 
-        ballsAndHoles(st.ballsAndHoles), 
-        horObstacles(st.horObstacles), 
-        vertObstacles(st.vertObstacles) { }
+State::State(const State& st) : State(st.width, st.height) {
+    balls = st.balls; 
+    holes = st.holes;
+}
 
 std::unique_ptr<State> State::move(Movement m) const {
     std::unique_ptr<State> st(new State(*this));
+    std::vector<dot_pair> transfers;
+
+    for (auto &k: st->balls) {
+        State::move(m, st, k.first);
+    }
 
     return st;
 }
 
-void State::addPair(dot ball, dot hole) {
-    ballsAndHoles[ballsAndHoles.size()] = std::make_pair(ball, hole);
+void State::addBall(const dot& ball) {
+    check(ball);
+    field[ball.first][ball.second] |= BALL;
+
+    balls[ball] = balls.size();
 }
 
-void State::addObstacle(dot_pair e) {
-    if (e.first.first == e.second.first) {
-        horObstacles.push_back(e);
-        return;
-    }
+void State::addHole(const dot& hole) {
+    check(hole);
+    field[hole.first][hole.second] |= HOLE;
+    
+    holes[hole] = holes.size();
+}
 
-    if (e.first.second == e.second.second) {
-        vertObstacles.push_back(e);
-        return;
-    }
+void State::addObstacle(const dot_pair& e) {
+    auto x0 = e.first.first;
+    auto y0 = e.first.second;
+    auto x1 = e.second.first;
+    auto y1 = e.second.second;
 
-    throw "Wrong obstacle";
+    setObstacle(x0, y0, x1, y1);
+}
+
+void State::check(const dot& d) const {
+    auto f = field[d.first][d.second];
+    if (BALL == f || HOLE == f)
+        throw "Tried to replace object";
+}
+
+void State::setObstacle(int32_t x0, int32_t y0, int32_t x1, int32_t y1) {
+    bool isVert = (x0 == x1);
+    if (!isVert) isVert = (y0 == y1);
+
+    if (!isVert) throw "Wrong obstacle";
+    
+    auto t1 = isVert ? OBST_DOWN : OBST_LEFT;
+    auto t2 = isVert ? OBST_UP : OBST_RIGHT;
+
+    bool upper = isVert ? (y0 > y1) : (x0 > x1);
+
+    if (upper) {
+        field[x0][y0] |= t2;
+        field[x1][y1] |= t1;
+    } else {
+        field[x0][y0] |= t1;
+        field[x1][y1] |= t2;
+    }
+}
+
+constexpr bool is_horizontal(Movement m) {
+    switch (m) {
+        case NORTH:
+        case SOUTH:
+            return false;
+        case EAST:
+        case WEST:
+            return true;
+    }
+}
+
+constexpr int32_t get_dx(Movement m) {
+    if (!is_horizontal(m)) return 0;
+    return EAST == m ? 1 : -1;
+}
+
+constexpr int32_t get_dy(Movement m) {
+    if (!is_horizontal(m)) return 0;
+    return NORTH == m ? 1 : -1;
+}
+
+void State::move(Movement m, std::unique_ptr<State> &st, const dot& d) {
+    const int32_t dx = get_dx(m);
+    const int32_t dy = get_dy(m);
+
+    dot final = d;
+
+
 }
--- src/table/State.h
+++ src/table/State.h
@@ -13,28 +13,46 @@ enum Movement {
     SOUTH
 };
 
+enum Cell {
+    NONE = 0,
+    HOLE = 1,
+    BALL = 2,
+    OBST_LEFT = 4,
+    OBST_RIGHT = 8,
+    OBST_UP = 16,
+    OBST_DOWN = 32
+};
+
 typedef std::pair<int32_t, int32_t> dot;
 typedef std::pair<dot, dot> dot_pair;
+typedef int32_t** matrix;
 
 class State {
 public:
     State(int32_t size);
+    State(int32_t w, int32_t h);
+    ~State();
     explicit State(const State&);
 
     // Perform rotation of the table
     std::unique_ptr<State> move(Movement) const;
 
-    void addPair(dot ball, dot hole);
-    void addObstacle(dot_pair);
+    void addBall(const dot&);
+    void addHole(const dot&);
+    void addObstacle(const dot_pair&);
 private:
+    matrix field;
 
-    // Index -> pair of dots, where first points to the ball, second to the hole
-    std::map<int, dot_pair> ballsAndHoles;
-    
-    std::vector<dot_pair> horObstacles;
-    std::vector<dot_pair> vertObstacles;
+    // Pair of dots, where first points to the ball, second to the hole -> index
+    std::map<dot, int> balls;
+    std::map<dot, int> holes;
 
     const int32_t width, height;
+
+    void check(const dot&) const;
+    void setObstacle(int32_t, int32_t, int32_t, int32_t);
+
+    static void move(Movement, std::unique_ptr<State>&, const dot&);
 };
 
 
